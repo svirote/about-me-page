2270aa8342f7667c91b2c519e352563053c1cc9a
svirote <svirote@users.noreply.github.com>
2022-09-29 22:47:49 +0200

    Folder organization

--- src/index.js
+++ src/index.js
@@ -1,47 +1,3 @@
-function navigationBar() {
-  let navBar = document.querySelector("#navigation");
-
-  navBar.innerHTML =
-    navBar.innerHTML +
-    `  
-   <nav class="navbar navbar-expand-lg navbar-theme fixed-top">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">@suvirote</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.html">
-                Home
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="about-me.html">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="projects.html">Projects</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="projects.html#contact">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    `;
-}
-
 function footer() {
   let footerText = document.querySelector("#footer-text");
 
@@ -75,5 +31,4 @@ function footer() {
     `;
 }
 
-navigationBar();
 footer();
